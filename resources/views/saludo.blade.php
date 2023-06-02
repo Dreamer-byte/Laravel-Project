6b0d4c7470884d39e5941d44e9fe5394e7946e6c
Dreamer-byte <parcestar@gmail.com>
2023-06-02 14:58:34 -0600

    Cambios en la vista saludo

--- resources/views/saludo.blade.php
+++ resources/views/saludo.blade.php
@@ -1,1 +1,4 @@
 <h1>Hola mundo desde Laravel</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quod asperiores earum doloremque magnam error quaerat, <br>
+     voluptas harum repellat sapiente maxime numquam non laboriosam pariatur, saepe eaque? Laborum, illum dignissimos?</p>
