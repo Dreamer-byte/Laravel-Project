0a083c4cb61fe4909a65f8b4ff01b0144151c68b
Dreamer-byte <parcestar@gmail.com>
2023-06-02 15:08:34 -0600

    agrgando contenido en vista saludo

--- resources/views/saludo.blade.php
+++ resources/views/saludo.blade.php
@@ -2,3 +2,6 @@
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quod asperiores earum doloremque magnam error quaerat, <br>
      voluptas harum repellat sapiente maxime numquam non laboriosam pariatur, saepe eaque? Laborum, illum dignissimos?</p>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit nostrum provident beatae voluptas ullam blanditiis laboriosam quaerat, ex architecto cum nam ducimus temporibus ipsum ut nobis quisquam laborum quas ratione.
+</p>
